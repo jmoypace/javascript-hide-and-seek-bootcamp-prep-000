d208b5a952be12c07486c13d7424762faaa00534
jmoypace <jmoypace@gmail.com>
2018-05-10 03:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nestedTarget(){
      //Error: Expected undefined to equal <div class="target">...</div>
 }
 function increaseRankBy(n){
-   var element = document.querySelectorAll('.ranked-list').innerHTML;
+   var element = document.getElementsByTagName('.ranked-list').innerHTML;
    for(var i=0;i<element.length;i++){
      element[i]=parseInt(element[i])+n;
    }
