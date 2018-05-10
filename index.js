f364d7b6687d2aecf2e72fcc03b602f383f0e1d2
jmoypace <jmoypace@gmail.com>
2018-05-10 03:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ for (let i = 0; i < lis.length; i++) {
 return lis;
 }
 function deepestChild(){
-  let element=document.querySelectorAll('div#grand-node');
+  let element=document.querySelectorAll('grand-node');
   return element[element.length-1];
 }
