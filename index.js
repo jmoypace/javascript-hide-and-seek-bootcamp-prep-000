b8200ad53f09f7f88cfe321e1233cb5d0bd8a980
jmoypace <jmoypace@gmail.com>
2018-05-10 02:46:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  
+ var element = document.querySelector(selector);
+ return element;
+ //index getFirstSelector(selector) returns the first element that matches theselector:
+  //element is not defined
+}
+function nestedTarget(){
+  var element=document.getElementsByTagName('div')[0];
+  return element;
+  //index nestedTarget() pulls a .target out of #nested:
+     //Error: Expected undefined to equal <div class="target">...</div>
+}
+function increaseRankBy(n){
+   var element = document.getElementById('#grand-node').querySelectorAll()
+   return element;
+}
+function deepestChild(){
+  
+}
